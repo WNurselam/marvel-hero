1362bd34c11a08567bacc3d041db4ac995bee401
Nurselam <77880259+WNurselam@users.noreply.github.com>
2022-12-30 15:55:13 +0300

    edit logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import { Image } from '@chakra-ui/react'
 function Header() {
   return (
     <Center>
-      <Image  src={Logo}  border="4px solid rgb(1, 22, 39)"  height="180px" width="1400px" />
+      <Image  src={Logo}  border="4px solid rgb(1, 22, 39)"  height="180px" width="1500px" />
     </Center>
   )
 }
